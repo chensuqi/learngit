c5df137076eb42015f850737f3b093c4834402f4
suqi <2337323984@qq.com>
2021-03-10 14:51:11 +0800

    Project/src/test/java/Test.java

--- Project/src/test/java/Test.java
+++ Project/src/test/java/Test.java
@@ -2,5 +2,12 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println(28D/10D);
+
+        String Str = new String("www.google.com");
+
+        System.out.print("匹配成功返回值 :" );
+//        System.out.println(Str.replaceAll("(.*)google(.*)", "runoob" ));
+        System.out.print("匹配失败返回值 :" );
+        System.out.println(Str.replaceAll("(.*)taobao(.*)", "runoob" ));
     }
 }
